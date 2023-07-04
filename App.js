76e698fe98fd1553ddd0aab0868e19a4ebca1af6
Muhammad Abubakar <muhammadabubakarrana5@gmail.com>
2023-07-04 17:16:02 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import Navigation from './src/navigation';
+import {RootSiblingParent} from 'react-native-root-siblings';
+
+export default function App() {
+  return (
+    <RootSiblingParent>
+      <SafeAreaProvider style={{flex: 1}}>
+        <Navigation />
+      </SafeAreaProvider>
+    </RootSiblingParent>
+  );
+}
